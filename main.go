cb73a34c40ffc8e76901aa14dcece8e685f7c875
ashpect <ashishndiitr@gmail.com>
2024-05-08 16:50:54 +0530

    cleanup

--- main.go
+++ main.go
@@ -5,35 +5,9 @@ import (
 	dfa "toc/automata"
 )
 
-// func main() {
-// 	d := dfa.NewDFA()
-// 	d.SetStates([]dfa.State{0, 1, 2})
-// 	d.SetInputAlphabet([]dfa.Symbol{0, 1, 2})
-// 	d.SetFinalState([]dfa.State{1})
-
-// 	d.AddTransitionFunctions(0, '0', []dfa.State{0})
-// 	d.AddTransitionFunctions(0, '1', []dfa.State{1})
-// 	d.AddTransitionFunctions(1, '0', []dfa.State{0})
-// 	d.AddTransitionFunctions(1, '1', []dfa.State{2})
-// 	d.AddTransitionFunctions(2, '0', []dfa.State{2})
-// 	d.AddTransitionFunctions(2, '1', []dfa.State{1})
-// 	d.AddTransitionFunctions(0, '2', []dfa.State{2,1})
-// 	d.AddGraph()
-// 	// read later on from a file
-// 	// sorting is pretty important
-
-// 	istrue, err := d.CheckTheLanguage("1001")
-// 	if istrue != true {
-// 		fmt.Println(err)
-// 	} else {
-// 		fmt.Println("Accepted")
-// 	}
-
-// 	fmt.Println(d.GetFinalState())
-// }
-
 func main() {
 
+	// --EXAMPLE USAGE--
 	// initiate dfa
 	d := dfa.NewDFA()
 
